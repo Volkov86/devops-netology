c3b41b303dbb429cdd0b75b0f030e3712d3c5dca
Konstantin Volkov <mc-volf2@yandex.ru>
2022-01-11 21:27:05 +0300

    Merge branch "git-rebase"

--- branching/rebase.sh
+++ branching/rebase.sh
@@ -2,9 +2,5 @@
 # display command line options
 
 count=1
-for param in "$@"; do
-    echo "\$@ Parameter #$count = $param"
-    count=$(( $count + 1 ))
+    echo "Next parameter: $param":
 done
-
-echo "====="
